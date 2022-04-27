8f77214c09a054bff50ea5aaee033e3b1cde2edf
Vitaly <w.182@mail.ru>
2022-04-28 00:03:08 +0300

    single item page design

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,9 +1,55 @@
+import Chart from '../../components/chart/Chart';
+import List from '../../components/table/Table';
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
 import './single.scss';
 
 const Single = () => {
     return (
-        <div>
-            Single
+        <div className='single'>
+            <Sidebar />
+            <div className="singleContainer">
+                <Navbar />
+                <div className="top">
+                    <div className="left">
+                        <div className="editButton">Edit</div>
+                        <h1 className="title">Information</h1>
+                        <div className="item">
+                            <img
+                                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                                alt=""
+                                className="itemImg"
+                            />
+                            <div className="details">
+                                <h1 className="itemTitle">Liza Crow</h1>
+                                <div className="detailItem">
+                                    <span className="itemKey">Email:</span>
+                                    <span className="itemValue">lizacrow@mail.com</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Phone:</span>
+                                    <span className="itemValue">+7 787 857 47 74</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Address:</span>
+                                    <span className="itemValue">Wall St. 555 NewYork</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Country:</span>
+                                    <span className="itemValue">India</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <Chart aspect={2.5/1} title='User Spending (Last 6 Months)'/>                        
+                    </div>
+                </div>
+                <div className="bottom">
+                    <h1 className="title">Last Transactions</h1>
+                    <List/>
+                </div>
+            </div>
         </div>
     );
 };
