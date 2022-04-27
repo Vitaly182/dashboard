43502e792bab0787974c662ae60832d12341c1d2
Vitaly <w.182@mail.ru>
2022-04-27 23:09:36 +0300

    datatable

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -17,8 +17,8 @@ const Chart = () => {
     return (
         <div className='chart'>
             <div className="title">Last 6 Months (Revenue)</div>
-            <ResponsiveContainer width="100%" aspect={3 / 1}>
-                <AreaChart width={730} height={250} data={data}
+            <ResponsiveContainer width="100%" height="100%">
+                <AreaChart width="100%" height="100%" data={data}
                     margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                     <defs>
                         <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
