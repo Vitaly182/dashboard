be2d28d129f905a79b421bbddcd755a892701552
Vitaly <w.182@mail.ru>
2022-04-28 22:51:57 +0300

    changes for deploy

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,8 +1,133 @@
 import './datatable.scss';
 import { DataGrid } from '@mui/x-data-grid';
-import { userColumns, userRows } from '../../datatableSource.js';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+
+
+const userColumns = [
+    { field: "id", headerName: "ID", width: 70 },
+    {
+        field: "user",
+        headerName: "User",
+        width: 230,
+        renderCell: (params) => {
+            return (
+                <div className="cellWithImg">
+                    <img className="cellImg" src={params.row.img} alt="avatar" />
+                    {params.row.username}
+                </div>
+            );
+        },
+    },
+    {
+        field: "email",
+        headerName: "Email",
+        width: 230,
+    },
+
+    {
+        field: "age",
+        headerName: "Age",
+        width: 100,
+    },
+    {
+        field: "status",
+        headerName: "Status",
+        width: 160,
+        renderCell: (params) => {
+            return (
+                <div className={`cellWithStatus ${params.row.status}`}>
+                    {params.row.status}
+                </div>
+            );
+        },
+    },
+];
+
+//temporary data
+export const userRows = [
+    {
+        id: 1,
+        username: "Snow",
+        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        status: "active",
+        email: "1snow@gmail.com",
+        age: 35,
+    },
+    {
+        id: 2,
+        username: "Jamie Lannister",
+        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "2snow@gmail.com",
+        status: "passive",
+        age: 42,
+    },
+    {
+        id: 3,
+        username: "Lannister",
+        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "3snow@gmail.com",
+        status: "pending",
+        age: 45,
+    },
+    {
+        id: 4,
+        username: "Stark",
+        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "4snow@gmail.com",
+        status: "active",
+        age: 16,
+    },
+    {
+        id: 5,
+        username: "Targaryen",
+        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "5snow@gmail.com",
+        status: "passive",
+        age: 22,
+    },
+    {
+        id: 6,
+        username: "Melisandre",
+        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "6snow@gmail.com",
+        status: "active",
+        age: 15,
+    },
+    {
+        id: 7,
+        username: "Clifford",
+        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "7snow@gmail.com",
+        status: "passive",
+        age: 44,
+    },
+    {
+        id: 8,
+        username: "Frances",
+        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "8snow@gmail.com",
+        status: "active",
+        age: 36,
+    },
+    {
+        id: 9,
+        username: "Roxie",
+        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "snow@gmail.com",
+        status: "pending",
+        age: 65,
+    },
+    {
+        id: 10,
+        username: "Roxie",
+        img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+        email: "snow@gmail.com",
+        status: "active",
+        age: 65,
+    },
+];
+
 
 
 const Datatable = () => {
@@ -10,7 +135,7 @@ const Datatable = () => {
     const [data, setData] = useState(userRows)
 
     const handleDelete = (id) => {
-        setData(data.filter((item) => item.id !== id ))
+        setData(data.filter((item) => item.id !== id))
     }
 
     const actionColumn = [
@@ -21,8 +146,8 @@ const Datatable = () => {
             renderCell: (params) => {
                 return (
                     <div className='cellAction'>
-                        <Link to='/users/test' style={{textDecoration: 'none'}} >
-                        <div className='viewButton'>View</div>
+                        <Link to='/users/test' style={{ textDecoration: 'none' }} >
+                            <div className='viewButton'>View</div>
                         </Link>
                         <div className='deleteButton' onClick={() => handleDelete(params.row.id)}>Delete</div>
                     </div>
@@ -35,7 +160,7 @@ const Datatable = () => {
         <div className='datatable'>
             <div className="datatableTitle">
                 Users
-                <Link to='/users/new'  className='link'>
+                <Link to='/users/new' className='link'>
                     Add New User
                 </Link>
             </div>
